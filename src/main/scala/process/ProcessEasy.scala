9c8fa140060b158a4a5fecc9b5951e3c63fe9b20
Jeroen Gordijn <jeroen.gordijn@gmail.com>
2015-02-12 21:21:42 +0100

    Now with state retrieval, no update yet

--- src/main/scala/process/ProcessEasy.scala
+++ src/main/scala/process/ProcessEasy.scala
@@ -2,10 +2,14 @@ package process
 
 import akka.actor._
 import akka.pattern.pipe
+import akka.util.Timeout
 import scala.concurrent.ExecutionContext
 import scala.concurrent.Future
 import scala.concurrent._
 import scala.concurrent.Promise
+import scala.reflect.ClassTag
+import scala.util.Failure
+import scala.util.Success
 
 object Main extends App {
   val system = ActorSystem("ProcessEasy")
@@ -14,28 +18,65 @@ object Main extends App {
   println("Started")
   Thread.sleep(500)
   process  ! "Start"
-  Thread.sleep(5000)
-  system.shutdown()
+  import system.dispatcher
+  //future {
+   // Thread.sleep(5000)
+   // system.shutdown()
+   //
+import scala.concurrent.duration._
+  try {system.awaitTermination(10 seconds)}
+  catch {
+    case _: TimeoutException =>
+    system.shutdown()
+  }
 
 }
 
-trait ProcessStep {
-  def promise: Promise[Unit] = Promise[Unit]()
-  def ~>(next: ProcessStep*): ProcessStep = new Chain(this, next:_*)
-  def execute()(implicit executionContext: ExecutionContext): Future[Any]
+object Process {
+  case class Perform[S](action: S ⇒ Unit)
+  case object GetState
+}
+
+trait ProcessStep[S] {
+  val promise: Promise[Unit] = Promise[Unit]()
+  val future = promise.future
+
+  def ~>(next: ProcessStep[S]*): ProcessStep[S] = {
+    new Chain(this, next:_*)
+  }
+  def execute(): S => Unit
+
+  def run()(implicit process: ActorRef, executionContext: ExecutionContext, classTag: ClassTag[S]): Future[Unit] = {
+    import akka.pattern.ask
+    import scala.concurrent.duration._
+    implicit val timeout: Timeout = 5 seconds
+
+    if (!promise.isCompleted) (process ? Process.GetState).mapTo[S].foreach(execute())
+    future
+  }
   def complete: PartialFunction[Any, Unit]
 }
 
-class Chain(a: ProcessStep, b: ProcessStep*) extends ProcessStep {
-  def execute()(implicit executionContext: ExecutionContext) = a.execute flatMap(_ => Future.sequence(b.map(_.execute)).map(_ => ()))
+class Chain[S](a: ProcessStep[S], b: ProcessStep[S]*) extends ProcessStep[S] {
+  override def run()(implicit self: ActorRef, executionContext: ExecutionContext, classTag: ClassTag[S]): Future[Unit] = {
+    a.run() flatMap{_ =>
+      Future.sequence(b.map(_.run())).flatMap{_ =>
+        promise.trySuccess(())
+        future
+      }
+    }
+  }
+
+  def execute() = throw new UnsupportedOperationException("Please invoke run")
+
   def complete: PartialFunction[Any, Unit] = a.complete orElse b.foldRight(PartialFunction.empty[Any, Unit]){case (x, y) => x.complete orElse y}
 }
 
-case class PrintStep(output: String, sleep: Long) extends ProcessStep {
-  def execute()(implicit executionContext: ExecutionContext) = future {
-    println(s"${new java.util.Date} Step is executed with output: $output ($sleep)")
+case class PrintStep(output: String, sleep: Long) extends ProcessStep[Int] {
+  def execute() = state => {
+    println(s"${new java.util.Date} Step is executed with output: $output ($sleep) STATE: $state")
     Thread.sleep(sleep)
-    promise.trySuccess(())
+    promise.success(())
   }
   def complete = PartialFunction.empty
 }
@@ -45,12 +86,22 @@ object WalkInThePark {
 }
 
 class WalkInThePark extends Actor {
-  var nrOfCalls = 0
-  def process = PrintStep("Stap 1", 500) ~> (PrintStep("Stap 2", 1500), PrintStep("Stap 3", 100)) ~> PrintStep("Done", 50)
   import context.dispatcher
+  var nrOfCalls = 0
+  val step1 = PrintStep("Stap 1", 500)
+  val step2 = PrintStep("FOOOOO", 300)
+
+  val process: ProcessStep[Int] =
+    step1 ~> step2 ~> (PrintStep("Stap 2", 1500), PrintStep("Stap 3", 100)) ~> PrintStep("Done", 50)
+
+  process.future.foreach{_ =>
+    context.system.shutdown()
+  }
 
   def receive = {
     case "Start" =>
-      process.execute()
+      println("START")
+      process.run()
+    case Process.GetState => sender() ! nrOfCalls
   }
 }
