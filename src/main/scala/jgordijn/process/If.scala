5d8093811903999547c0d9893867c9092235c1d6
Jeroen Gordijn <jeroen.gordijn@gmail.com>
2015-04-21 10:58:17 +0200

    Fix for if else

--- src/main/scala/jgordijn/process/If.scala
+++ src/main/scala/jgordijn/process/If.scala
@@ -1,15 +1,12 @@
 package jgordijn.process
 
-import scala.concurrent.duration.Duration
-import scala.concurrent.{ ExecutionContext, Future, Promise }
 import scala.reflect.ClassTag
 
-import akka.actor.{ Actor, ActorContext, ActorRef, Props }
-import akka.util.Timeout
+import akka.actor.ActorContext
 
 object If {
   def apply[S](condition: S => Boolean)(process: ProcessStep[S])(implicit context: ActorContext, classTag: ClassTag[S]) = new If(condition)(process)
 }
 class If[S](condition: S => Boolean)(process: ProcessStep[S])(implicit context: ActorContext, classTag: ClassTag[S]) extends Choice[S](condition, process, EmptyStep()) {
-  def Else(elseProcess: ProcessStep[S])(implicit context: ActorContext, classTag: ClassTag[S]) = new Choice[S](condition, EmptyStep(), elseProcess)
+  def Else(elseProcess: ProcessStep[S])(implicit context: ActorContext, classTag: ClassTag[S]) = new Choice[S](condition, process, elseProcess)
 }
