205a556c1d8576cd8f9529686c36b75d9a35c166
Jeroen Gordijn <jeroen.gordijn@gmail.com>
2015-04-24 10:05:49 +0200

    Enhanched test

--- src/test/scala/jgordijn/process/PersistentProcessTest.scala
+++ src/test/scala/jgordijn/process/PersistentProcessTest.scala
@@ -14,55 +14,72 @@ import org.scalatest.concurrent.Eventually
 
 object PersistentProcessTest {
   case object Start
-  case object Response
-  case class Command(i: Int)
-  case object Completed extends Process.Event
-  class Step(probe: ActorRef)(implicit val context: ActorContext) extends ProcessStep[Int] {
-    def execute()(implicit process: akka.actor.ActorRef): Execution = { state =>
+  case class Response(probe: ActorRef)
+  case class Command(i: PersistentProcess1.State)
+  case class Completed(probe: String) extends Process.Event
+  class Step(probe: ActorRef)(implicit val context: ActorContext) extends ProcessStep[PersistentProcess1.State] {
+    def execute()(implicit process: akka.actor.ActorRef): Execution = { state ⇒
+      println(s"${probe.path.name}: exec")
       probe ! Command(state)
     }
     def receiveCommand: CommandToEvent = {
-      case Response =>
-        Completed
+      case Response(`probe`) ⇒
+        println(s"${probe.path.name}: complete")
+        Completed(probe.path.name)
     }
     def updateState: UpdateFunction = {
-      case Completed => { state =>
+      case Completed(p) if p == probe.path.name ⇒ { state ⇒
         markDone()
-        state + 1
+        val newState = state.copy(probeCalled = probe :: state.probeCalled)
+        println(s"${probe.path.name}: newState: ${newState.probeCalled.map(_.path.name)}")
+        newState
       }
     }
+    override def toString: String = s"Step: ${probe.path.name}"
+
   }
 
-  class InitStep()(implicit val context: ActorContext) extends ProcessStep[Int] {
-    def execute()(implicit process: akka.actor.ActorRef): Execution = { state =>
+  class InitStep()(implicit val context: ActorContext) extends ProcessStep[PersistentProcess1.State] {
+    def execute()(implicit process: akka.actor.ActorRef): Execution = { state ⇒
     }
-    def receiveCommand: PartialFunction[Any,Process.Event] = {
-      case Start =>
-        Completed
+    def receiveCommand: PartialFunction[Any, Process.Event] = {
+      case Start ⇒
+        println("complete init")
+        Completed("init")
     }
 
     def updateState: UpdateFunction = {
-      case Completed => { state =>
+      case Completed("init") ⇒ { state ⇒
         markDone()
+        println("init completed")
         state
       }
     }
+
+    override def toString: String = "Init"
   }
 
-
-  class PersistentProcess1(probe1: TestProbe, probe2: TestProbe, probe3: TestProbe, probe4: TestProbe, endProbe: TestProbe, completeHook: TestProbe) extends PersistentProcess[Int] {
+  object PersistentProcess1 {
+    case class State(probeCalled: List[ActorRef] = Nil)
+  }
+  class PersistentProcess1(probe1: TestProbe, probe2: TestProbe, probe3: TestProbe, probe4: TestProbe, probe5: TestProbe, endProbe: TestProbe, completeHook: TestProbe) extends PersistentProcess[PersistentProcess1.State] {
     import context.dispatcher
     val persistenceId = "PersistentProcess1"
 
-    var state = 0
-    val process = new InitStep() ~> new Step(probe1.ref) ~> new Choice(state => state == 1, new Step(probe2.ref) ~> new Step(probe3.ref) , new Step(probe4.ref)) ~> new Step(endProbe.ref)
+    var state = PersistentProcess1.State()
+    val process = new InitStep() ~>
+      Par(new Step(probe1.ref), new Step(probe2.ref)) ~>
+      new Choice(state ⇒ state.probeCalled.contains(probe1.ref),
+        new Step(probe3.ref) ~> new Step(probe4.ref),
+        new Step(probe5.ref)) ~>
+      new Step(endProbe.ref)
 
-    process.onComplete { case (context, state) =>
-      completeHook.ref ! s"DONE-$state"
+    process.onComplete {
+      case (context, state) ⇒
+        completeHook.ref ! s"DONE-${state.probeCalled.size}"
     }
   }
 }
-
 
 class PersistentProcessTest extends TestKit(ActorSystem("ProcessStepTest"))
     with ImplicitSender
@@ -80,12 +97,26 @@ class PersistentProcessTest extends TestKit(ActorSystem("ProcessStepTest"))
   val probe2 = TestProbe()
   val probe3 = TestProbe()
   val probe4 = TestProbe()
+  val probe5 = TestProbe()
   val endProbe = TestProbe()
   val completeHookProbe = TestProbe()
 
-  def assertStateIs[State](process: ActorRef, state: State) = {
+  val probes =
+    s"""|probe1: ${probe1.ref.path.name}
+       |probe2: ${probe2.ref.path.name}
+       |probe3: ${probe3.ref.path.name}
+       |probe4: ${probe4.ref.path.name}
+       |probe5: ${probe5.ref.path.name}
+       |endPre: ${endProbe.ref.path.name}
+       |hook  : ${completeHookProbe.ref.path.name}
+       |\n""".stripMargin
+
+  println(probes)
+
+  def assertStateContains[State](process: ActorRef, probe: ActorRef) = {
     process ! Process.GetState
-    expectMsg(state)
+    val msg = expectMsgType[PersistentProcess1.State]
+    msg.probeCalled should contain(probe)
   }
   def stop(actor: ActorRef) {
     watch(actor)
@@ -93,34 +124,38 @@ class PersistentProcessTest extends TestKit(ActorSystem("ProcessStepTest"))
     expectTerminated(actor)
   }
 
+  implicit class TestProbeForStep(testProbe: TestProbe) {
+    def expect(process: ActorRef): Unit = {
+      val msg = testProbe.expectMsgType[PersistentProcessTest.Command]
+      println(s"${testProbe.ref.path.name}: ${msg.i.probeCalled.map(_.path.name)}")
+      testProbe.reply(Response(testProbe.ref))
+      eventually {
+        assertStateContains(process, testProbe.ref)
+      }
+    }
+  }
+
   "PersistentProcess" should {
     "persist state" in {
-      val process = system.actorOf(Props(new PersistentProcess1(probe1, probe2, probe3, probe4, endProbe, completeHookProbe)), "persisted-process-test1")
-      assertStateIs(process, 0)
+      val process = system.actorOf(Props(new PersistentProcess1(probe1, probe2, probe3, probe4, probe5, endProbe, completeHookProbe)), "persisted-process-test1")
+      //assertStateIs(process, 0)
       process ! Start
-      probe1.expectMsg(Command(0))
-      probe1.reply(Response)
-      eventually {
-        assertStateIs(process, 1)
+      probe1.expect(process)
+      probe2.expect(process)
+      probe3.expectMsgPF(3 second) {
+        case _: Command ⇒ println("Going to stop the process now")
       }
-      probe2.expectMsg(Command(1))
       stop(process)
-      val newProcess = system.actorOf(Props(new PersistentProcess1(probe1, probe2, probe3, probe4, endProbe, completeHookProbe)), "persisted-process-test1")
-      assertStateIs(newProcess, 1)
-      probe2.expectMsg(Command(1))
-      probe2.reply(Response)
-      eventually {
-        assertStateIs(newProcess, 2)
+      val newProcess = system.actorOf(Props(new PersistentProcess1(probe1, probe2, probe3, probe4, probe5, endProbe, completeHookProbe)), "persisted-process-test1")
+      //assertStateIs(newProcess, 1)
+      probe3.expect(newProcess)
+      probe4.expect(newProcess)
+      endProbe.expectMsgPF(1 second) {
+        case _: Command ⇒
       }
-      probe3.expectMsg(Command(2))
-      probe3.reply(Response)
-      eventually {
-        assertStateIs(newProcess, 3)
-      }
-      endProbe.expectMsg(Command(3))
-      probe3.reply(Response)
-      probe4.expectNoMsg(250 millis)
-      completeHookProbe.expectMsg("DONE-4")
+      endProbe.reply(Response(endProbe.ref))
+      probe5.expectNoMsg(250 millis)
+      completeHookProbe.expectMsg("DONE-5")
     }
   }
 
