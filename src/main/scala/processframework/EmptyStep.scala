9e52f5df5c694a2617a92bad8264be5a9427d4ff
Gerard van de Glind <gvdglind@wehkamp.nl>
2015-05-13 22:46:58 +0200

    Improvements: mark empty step done directly & added spec for it

--- src/main/scala/processframework/EmptyStep.scala
+++ src/main/scala/processframework/EmptyStep.scala
@@ -3,10 +3,12 @@ package processframework
 import akka.actor.{ ActorContext, ActorRef }
 
 object EmptyStep {
-  def apply[S]()(implicit context: ActorContext): ProcessStep[S] = new EmptyStep()
+  def apply[S]()(implicit context: ActorContext): EmptyStep[S] = new EmptyStep()
 }
 class EmptyStep[S]()(implicit val context: ActorContext) extends ProcessStep[S] {
-  override def execute()(implicit process: ActorRef): Execution = {_ => markDone()}
+  markDone()
+
+  override def execute()(implicit process: ActorRef): Execution = { _ => }
   override def updateState: UpdateFunction = PartialFunction.empty
   override def receiveCommand: CommandToEvent = PartialFunction.empty
 }
---
+++ src/test/scala/processframework/EmptyStepSpec.scala
@@ -0,0 +1,44 @@
+package processframework
+
+import akka.actor.ActorContext
+import akka.testkit.TestProbe
+
+import scala.reflect.ClassTag
+
+case class NoState()
+
+class EmptyStepSpec extends BaseSpec with ProcessStepTestSupport[NoState, EmptyStep[NoState]] {
+
+  def createTestProbe: TestProbe = TestProbe()
+
+  def createProcessStep(executeProbe: TestProbe)(implicit context: ActorContext): EmptyStep[NoState] = EmptyStep[NoState]()
+
+  "EmptyStep" should {
+    "be completed directly" in {
+      val emptyStep = processStep()
+      emptyStep.isCompleted shouldBe true
+    }
+    "be executable" in {
+      val emptyStep = processStep()
+      emptyStep.execute()
+    }
+    "give a MatchError when receiving a command" in {
+      val emptyStep = processStep()
+      intercept[MatchError] {
+        emptyStep.receiveCommand(ProcessStepTestSupport.ACommand)
+      }
+      intercept[MatchError] {
+        emptyStep.handleReceiveCommand(ProcessStepTestSupport.ACommand)
+      }
+    }
+    "give a MatchError when updating state for an event" in {
+      val emptyStep = processStep()
+      intercept[MatchError] {
+        emptyStep.updateState(ProcessStepTestSupport.AnEvent)
+      }
+      intercept[MatchError] {
+        emptyStep.handleUpdateState(ProcessStepTestSupport.AnEvent)
+      }
+    }
+  }
+}
