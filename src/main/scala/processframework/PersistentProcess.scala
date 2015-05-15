886fd02981b57929653811c539421753cf1bc9f9
Gerard van de Glind <gvdglind@wehkamp.nl>
2015-05-15 14:01:25 +0200

    ImprovementsII: added logging to persistent process

--- src/main/scala/processframework/PersistentProcess.scala
+++ src/main/scala/processframework/PersistentProcess.scala
@@ -1,7 +1,7 @@
 package processframework
 
 import akka.actor.Actor._
-import akka.actor.{Actor, ActorContext}
+import akka.actor.{ActorLogging, Actor, ActorContext}
 import akka.persistence.{PersistentActor, RecoveryCompleted}
 import processframework.Process.{AbortCommand, AbortEvent}
 
@@ -14,7 +14,6 @@ object PersistentProcess {
 trait Customizable {
   def eventHandling: Receive = Actor.emptyBehavior
   def commandHandling: Receive = Actor.emptyBehavior
-
 }
 
 trait AbortablePersistentProcess[S] extends PersistentProcess[S] {
@@ -34,7 +33,7 @@ trait AbortablePersistentProcess[S] extends PersistentProcess[S] {
   }
 }
 
-abstract class PersistentProcess[State : ClassTag] extends PersistentActor {
+abstract class PersistentProcess[State : ClassTag] extends PersistentActor with ActorLogging {
   def process: ProcessStep[State]
   var state: State
 
@@ -47,6 +46,7 @@ abstract class PersistentProcess[State : ClassTag] extends PersistentActor {
     case event: Process.Event if process.handleUpdateState.isDefinedAt(event) =>
       state = process.handleUpdateState(event)(state)
     case event: Process.Event =>
+      log.warning(s"Persistent process (${this.getClass.getSimpleName}): unhandled event during recovery, event='$event'")
       unhandledRecoveryEvent(event)
     case RecoveryCompleted =>
       import context.dispatcher
@@ -58,19 +58,25 @@ abstract class PersistentProcess[State : ClassTag] extends PersistentActor {
 
   override def unhandled(msg: Any): Unit = msg match {
     case x if commandHandling.isDefinedAt(x) =>
+      log.debug(s"Persistent process (${this.getClass.getSimpleName}): commandHandling handles command '$x'")
       commandHandling(x)
     case x if process.handleReceiveCommand.isDefinedAt(x) =>
       val event = process.handleReceiveCommand(x)
+      log.debug(s"Persistent process (${this.getClass.getSimpleName}): handled command '$x', resulted in event '$event'")
       self ! event
     case event: Process.Event =>
       persist(event) { event =>
+        log.debug(s"Persistent process (${this.getClass.getSimpleName}): persisted event '$event'")
         state = process.handleUpdateState(event)(state)
       }
     case processframework.Process.GetState =>
+      log.debug(s"Persistent process (${this.getClass.getSimpleName}): get state '$state'")
       sender() ! state
     case perform: PersistentProcess.Perform[State] =>
+      log.debug(s"Persistent process (${this.getClass.getSimpleName}): performing action, '${perform.action}'")
       perform.action(context, state)
     case m =>
+      log.debug(s"Persistent process (${this.getClass.getSimpleName}): persistent process, unhandled msg: '$m'")
       super.unhandled(m)
   }
 }
